e2fc7a597a84b3f9bccfee392c9cc3dfe84fd365
abhimanyu ahuja <abhimanyu.ahuja@rooter.io>
2024-03-15 23:21:56 +0530

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import router from './routes/index.js'
 import { connectToMongoDb } from './config/mongoose.config.js';
+import cors from 'cors';
 
 const setupAndStartServer = async () => {
     const app = express();
+    app.use(cors());
     const PORT = process.env.PORT || 3000;
     await connectToMongoDb();
     app.use('',router);
